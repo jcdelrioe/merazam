2243aa6fe648bba2e7d55ee74987c27451998faa
Juan Carlos Del Rio <jcdre@hotmail.com>
2023-10-22 15:57:45 -0500

    Init project, add logo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import mzLogo from './assets/Logo MZ Up-Dw Tiny.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+        <a href="#" target="_blank">
+          <img src={mzLogo} className="logo react" alt="Merazam logo" />
+        </a>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
