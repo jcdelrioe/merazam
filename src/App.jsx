0d4be27bb8b4e4321ba23a4506ca8f7e41649395
Juan Carlos Del Rio <jcdre@hotmail.com>
2023-10-22 17:01:03 -0500

    add lema width & color

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="containerLogo">
 
         <a href="#" target="_blank">
           <img src={mzLogo} className="logo react" alt="Merazam logo" />
         </a>
-        <h3>La alianza perfecta para la consolidación de
+        <h3 className="logoLema">La alianza perfecta para la consolidación de
 grandes proyectos de ingeniería con alta calidad</h3>
       </div>
 
