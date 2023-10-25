fc635286deaac6e10a9378e8053c080795e37e5c
Juan Carlos Del Rio <jcdre@hotmail.com>
2023-10-24 20:27:49 -0500

    fix some bugs, add background-image

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import picFondo from './assets/RM4.png'
 import { Header } from "./Components/Header/Header";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Inicio } from "./Components/Inicio/Inicio";
@@ -7,10 +8,12 @@ import { Mision } from "./Components/Mision/Mision";
 function App() {
   return (
     <>
+
       <Header/>
       <Navbar/>
       <Inicio/>
       <Mision/>
+
 
     </>
   );
--- src/Components/Mision/Mision.jsx
+++ src/Components/Mision/Mision.jsx
@@ -4,12 +4,17 @@ import merazamLogo from "../../assets/Logo MZUp-DwTiny.png";
 export const Mision = () => {
   return (
     <section className="containerLogo" id="MISION">
-      <a href="#" target="_blank">
+      {/* <a href="#" target="_blank">
         <img src={merazamLogo} className="logo" alt="Merazam logo" />
-      </a>
-      <h3 className="logoLema">
-        Ser una empresa lider en la construcción, ofreciendo soluciones integrales a nuestros clientes, con un equipo humano altamente calificado y comprometido con la calidad, seguridad y medio ambiente
-      </h3>
+      </a> */}
+      <h3>Visión</h3>
+      <p className="logoLema">
+        Ser una empresa lider en la construcción, ofreciendo soluciones integrales a nuestros clientes, con un equipo humano altamente calificado y comprometido con la calidad, seguridad y medio ambiente.
+      </p>
+      <h3>Misión</h3>
+      <p className="logoLema">
+        Aportar soluciones innovadoras en construcci[on, que contribuyan al desarrollo sostenible de nuestros clientes y del país.]
+      </p>
     </section>
   );
 };
