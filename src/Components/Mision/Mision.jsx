9b4e772d4baf1cc03fd4f1b00f5222d9f2a12588
Juan Carlos Del Rio <jcdre@hotmail.com>
2023-10-28 12:41:57 -0500

    Inicio changes

--- src/Components/Mision/Mision.jsx
+++ src/Components/Mision/Mision.jsx
@@ -7,11 +7,11 @@ export const Mision = () => {
       {/* <a href="#" target="_blank">
         <img src={merazamLogo} className="logo" alt="Merazam logo" />
       </a> */}
-      <h3>Visión</h3>
+      <h3 className="subTittle">Visión</h3>
       <p className="logoLema">
         Ser una empresa líder en la construcción, ofreciendo soluciones integrales a nuestros clientes, con un equipo humano altamente calificado y comprometido con la calidad, seguridad y medio ambiente.
       </p>
-      <h3>Misión</h3>
+      <h3 className="subTittle">Misión</h3>
       <p className="logoLema">
         Aportar soluciones innovadoras en construcción, que contribuyan al desarrollo sostenible de nuestros clientes y del país.
       </p>
