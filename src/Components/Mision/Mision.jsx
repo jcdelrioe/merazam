dcdbfbc29bbdb0edd4f11ee8b060833a4b338c0a
Juan Carlos Del Rio <jcdre@hotmail.com>
2023-10-28 12:35:31 -0500

    view

--- src/Components/Mision/Mision.jsx
+++ src/Components/Mision/Mision.jsx
@@ -9,12 +9,16 @@ export const Mision = () => {
       </a> */}
       <h3>Visión</h3>
       <p className="logoLema">
-        Ser una empresa lider en la construcción, ofreciendo soluciones integrales a nuestros clientes, con un equipo humano altamente calificado y comprometido con la calidad, seguridad y medio ambiente.
+        Ser una empresa líder en la construcción, ofreciendo soluciones integrales a nuestros clientes, con un equipo humano altamente calificado y comprometido con la calidad, seguridad y medio ambiente.
       </p>
       <h3>Misión</h3>
       <p className="logoLema">
-        Aportar soluciones innovadoras en construcci[on, que contribuyan al desarrollo sostenible de nuestros clientes y del país.]
+        Aportar soluciones innovadoras en construcción, que contribuyan al desarrollo sostenible de nuestros clientes y del país.
       </p>
+      <h4 className="logoLema">
+        La alianza perfecta para la consolidación de grandes proyectos de
+        ingeniería con alta calidad
+      </h4>
     </section>
   );
 };
