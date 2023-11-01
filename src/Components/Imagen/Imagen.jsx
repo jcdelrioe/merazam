7059f86025b79a77df78d808b6d2f4376ca94427
Juan Carlos Del Rio <jcdre@hotmail.com>
2023-10-31 19:10:21 -0500

    change Imagen alt

--- src/Components/Imagen/Imagen.jsx
+++ src/Components/Imagen/Imagen.jsx
@@ -5,7 +5,7 @@ export const Imagen = ({images}) => {
   return (
     <div className="slider">
       {images.map((image) => (
-        <img key={images.key} className="sliderImg" src={image.src} alt={image.description} />
+        <img key={images.key} className="sliderImg" src={image.src} alt="" />
       ))}
     </div>
   );
